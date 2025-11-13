69f26baf30f9d09867c11deb92b271cbefbfd34b
tmon <kimhseung158@gmail.com>
2025-11-13 17:11:26 +0900

    [프로그래머스] H-Index / 정렬 (Java)

---
+++ solve/hyunseung/programmers/[000000] H-index.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] citations = { 3, 0, 6, 1, 5 };
+        int result = s.solution(citations);
+
+        System.out.println(result); // 기대값: 3
+    }
+}
